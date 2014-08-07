16533afbb69527afc60495d832e8b5094014bf73
Piotr Lizończyk <piotr.lizonczyk@deployed.pl>
2014-08-07 15:30:56 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+permalink_adder
+=======
+SEO tool for adding permalinks to text contained in Django apps databases.
+
+# Installation
+Install with pip:
+`pip install permalink_adder'
+
+# Usage
+todo
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'permalink_adder',
+  packages = ['permalink_adder'], # this must be the same as the name above
+  version = '0.1',
+  description = 'SEO tool for adding permalinks to text contained in Django apps databases.',
+  author = 'Piotr Lizonczyk',
+  author_email = 'piotr.lizonczyk@gmail.com',
+  url = 'https://github.com/plizonczyk/permalink_adder', # use the URL to the github repo
+  download_url = 'https://github.com/plizonczyk/permalink_adder/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
