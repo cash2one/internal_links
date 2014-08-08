f2bfe262c0149c299e41e16b7a90f26ab9c5a8e6
Piotr Lizończyk <piotr.lizonczyk@deployed.pl>
2014-08-08 09:14:16 +0200

    Setup.py fix

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+# coding=utf-8
 from distutils.core import setup
 setup(
   name = 'permalink_adder',
-  packages = ['permalink_adder'], # this must be the same as the name above
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'SEO tool for adding permalinks to text contained in Django apps databases.',
-  author = 'Piotr Lizonczyk',
+  author = u'Piotr Lizończyk',
   author_email = 'piotr.lizonczyk@gmail.com',
-  url = 'https://github.com/plizonczyk/permalink_adder', # use the URL to the github repo
-  download_url = 'https://github.com/plizonczyk/permalink_adder/tarball/0.1', # I'll explain this in a second
-  keywords = ['SEO', 'permalinks'], # arbitrary keywords
+  url = 'https://github.com/plizonczyk/permalink_adder',
+  download_url = 'https://github.com/plizonczyk/permalink_adder/tarball/0.1',
+  keywords = ['SEO', 'permalinks'],
   classifiers = ['Development Status :: 2 - Pre-Alpha',
                  'Framework :: Django',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 2.7'],
-  requires = ['django']
+  requires = ['django'],
+  packages = ['permalink_adder',
+              'permalink_adder.management',
+              'permalink_adder.management.commands'],
 )
