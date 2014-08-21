b6571183b0a6b1fcad4f5763e7aaff928c10453f
Piotr Lizończyk <piotr.lizonczyk@deployed.pl>
2014-08-21 12:03:40 +0200

    Some refactoring and tests added.

--- internal_links/helpers.py
+++ internal_links/helpers.py
@@ -1,8 +1,8 @@
 import re
-from django.db.models.loading import get_model
 
 
 def collect_text(app, model_name, fields):
+    from django.db.models.loading import get_model
     texts = []
     model = get_model(app, model_name)
     if not fields:
@@ -26,28 +26,46 @@ def collect_text(app, model_name, fields):
     return texts
 
 
-def find_text_occurrences(word, text):
+def find_text_occurrences(word, text_dict):
     matches = []
-    if len(word) > 0:
-        rule = r'( |^)%s([ .,?!:;]|$)' % word
+    if not len(word) > 0:
+        return matches
 
-        match = re.finditer(rule, text['content'], re.UNICODE | re.IGNORECASE)
-        for item in match:
-            print 'Found "%s" on %s in model %s, field "%s". Context: "%s"' % (item.group(),
-                                                       item.span(), text['model'], text['field'],
-                                                       text['content'][item.start() - 30 if item.start() > 30
-                                                                                         else 0 :item.end() + 30])
-            matches.append({'word': item.group().strip(' .,?!:;'),
-                            'start': item.start() if item.group()[0].isalnum() else item.start() + 1,
-                            'end': item.end() if item.group()[-1].isalnum() else item.end() - 1})
+    content = text_dict.get('content', '')
+    model = text_dict.get('model', '')
+    field = text_dict.get('field', '')
+
+    rule = r'( |^)%s([ .,?!:;]|$)' % word
+
+    match = re.finditer(rule, content, re.UNICODE | re.IGNORECASE)
+    for item in match:
+        print 'Found "%s" on %s in model %s, field "%s". Context: "%s"' % (item.group(),
+                                                   item.span(), model, field,
+                                                   content[item.start() - 30 if item.start() > 30
+                                                                                     else 0 :item.end() + 30])
+        matches.append({'word': item.group().strip(' .,?!:;'),
+                        'start': item.start() if item.group()[0].isalnum() else item.start() + 1,
+                        'end': item.end() if item.group()[-1].isalnum() else item.end() - 1})
 
     return matches
 
 
-def insert_links_to_text(text, matches, url, target):
+def insert_links_to_text(text, matches, url, target=False, max_occurrence=-1):
+    """
+
+    :param text:
+    :param matches:
+    :param url:
+    :param target:
+    :param max_occurrence:
+    :return:
+    """
     offset = 0
 
-    for match in matches:
+    for occurrence, match in enumerate(matches):
+        if occurrence == max_occurrence:
+            break
+
         prefix = text['content'][:match['start'] + offset]
         suffix = text['content'][match['end'] + offset:]
 
@@ -62,17 +80,28 @@ def insert_links_to_text(text, matches, url, target):
         text['content'] = prefix + prefix_tag + match['word'] + suffix_tag + suffix
 
         text['modified'] = True
-
     return text
 
 
-def add_links(app, model_name, fields, words, url, target, dry_run=True):
+def add_links(app, model_name, fields, words, url, target=False, occurrence=-1, dry_run=True):
+    """
+
+    :param app:
+    :param model_name:
+    :param fields:
+    :param words:
+    :param url:
+    :param target:
+    :param occurrence:
+    :param dry_run:
+    :return:
+    """
     texts = collect_text(app, model_name, fields)
     for word in words:
         for text in texts:
             matches = find_text_occurrences(word, text)
             objects = text['model'].objects.filter(pk=text['object_pk'])
-            text = insert_links_to_text(text, matches, url, target)
+            text = insert_links_to_text(text, matches, url, target, occurrence)
             if not dry_run:
                 for item in objects:
                     setattr(item, text['field'], text['content'])
